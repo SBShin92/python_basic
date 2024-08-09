ffdd305eb0d73c21f196bddaff8c161a9a8673d9
SBShin92 <aopaoi0987@gmail.com>
2024-08-09 16:09:36 +0900

    study operator overloading

--- python_advanced/point.py
+++ python_advanced/point.py
@@ -1,5 +1,12 @@
 class Point:
-    instance_count = []  # 클래스 멤버
+    instance_count = 0  # 클래스 멤버
+
+    def __init__(self, x=0, y=0) -> None:
+        self.x, self.y = x, y
+        self.instance_count += 1
+
+    def __del__(self):
+        self.instance_count -= 1
 
     def set_x(self, x):
         self.x = x
@@ -13,5 +20,74 @@ class Point:
     def get_y(self):
         return self.y
     
+    def __str__(self):
+        return f"Point x = {self.x}, y = {self.y}"
     
+    def __repr__(self) -> str:
+        return f"Point({self.x}, {self.y})"
+    
+    def __add__(self, other):  # Point + other
+        if isinstance(other, int):
+            self.x += other
+            self.y += other
+        elif isinstance(other, Point):  # Point + Point
+            self.x += other.get_x()
+            self.y += other.get_y()
+        return self
+    
+    def __radd__(self, other):  # other + Point
+        return self + other
+    
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+        return False
 
+
+    # 연습문제: 산술연산자, 역이항연산자, 비교연산자 중
+    # 몇개 오버로딩 해보셈
+
+    def __lshift__(self, other):
+        if isinstance(other, Point):
+            print(f"좌항: Point({self.x}, {self.y})")
+            print(f"우항: Point({other.x}, {other.y})")
+
+    
+    def __rlshift__(self, other):
+        return self << other
+
+    def __le__(self, other):
+        if isinstance(other, Point):
+            return self.x <= other.x
+        
+    
+    def __ge__(self, other):
+        if isinstance(other, Point):
+            return self.y >= other.y
+
+
+
+    # 싱글톤 예제
+
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if Singleton.__instance is not None:
+            raise Exception("싱글톤 인스턴스는 이미 생성됐습니다.")
+        else:
+            Singleton.__instance = self
+            print("싱글톤 인스턴스 생성 완료")
+
+    @classmethod
+    def get_class_instance(cls):
+        if cls.__instance is None:
+            cls()
+        return cls.__instance
+    
+    @staticmethod
+    def get_static_instance():
+        if Singleton.get_class_instance() is None:
+            Singleton()
+        return Singleton.get_class_instance()
+    
--- python_advanced/point_test.py
+++ python_advanced/point_test.py
@@ -20,12 +20,12 @@ def unbound_instance_method():
 
 def class_member_test():
     p1 = Point()
-    p1.set_x([])
+    p1.set_x(50)
     p1.set_y(60)
     print(p1.get_x(), p1.get_y(), p1.instance_count)
 
     p2 = Point()
-    p2.set_x([])
+    p2.set_x(50)
     p2.set_y(60)
     print(p2.get_x(), p2.get_y(), p2.instance_count)
 
@@ -33,7 +33,55 @@ def class_member_test():
     print(p1.instance_count is p2.instance_count)
 
 
+def stringify():
+    p = Point(10, 20)
+    print(p)
+    print(repr(p))  # 원래 객체를 재현할 수 있는 문자열 출력
+
+    p_repr = eval(repr(p))  # 이렇게
+    print(p_repr)
+
+
+from point import Singleton
+
+
+def singleton_test():
+    s = Singleton()
+    print(s)
+    # s2 = Singleton()
+    s2 = Singleton.get_class_instance()
+    print(s2)
+    s3 = Singleton.get_static_instance()
+    print(s3)
+
+
+# 연습문제: 산술연산자, 역이항연산자, 비교연산자 중
+# 몇개 오버로딩 해보셈
+def oper_overload():
+    p = Point(10, 20)
+    print(p + 10)
+    print(p + Point(10, 20))
+
+    print(10 + p)
+    print(p)
+
+    p1 = Point(5, 20)
+    p2 = Point(10, 15)
+    print(p1 == p2)
+
+    p1 << Point(15, 25)
+    print(p1 >= p2)
+    print(p1 <= p2)
+
+
+
+
+
+
 if __name__ == '__main__':
     # bound_instance_method()
     # unbound_instance_method()
-    class_member_test()
+    # class_member_test()
+    # stringify()
+    # singleton_test()
+    oper_overload()
